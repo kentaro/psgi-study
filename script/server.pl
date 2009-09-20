b599da5ef64d2332bfbe419bd60229fc505a0e9d
kentaro <kentaro@nksk.(none)>
2009-09-21 00:44:25 +0900

    server.plをちょっと変更

--- script/server.pl
+++ script/server.pl
@@ -10,10 +10,10 @@ use lib glob "$FindBin::Bin/../modules/*/lib";
 use Plack::Loader;
 use Example::Handler::PSGI;
 
-GetOptions(\my %opts, "impl=s", "help");
+GetOptions(\my %opts, "app=s", "impl=s", "help");
 $ENV{PLACK_IMPL} = $opts{impl} if $opts{impl};
 
-my $handler = Example::Handler::PSGI->handler('MyApp');
+my $handler = Example::Handler::PSGI->handler(delete $opts{app} || 'MyApp');
 my $impl    = Plack::Loader->auto(%opts);
    $impl->run($handler);
    $impl->run_loop if $impl->can('run_loop');
